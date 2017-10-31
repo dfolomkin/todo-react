a7017dea5237c95a5a51503bdc2c91ce72f73ce4
dfolomkin <d.folomkin85@gmail.com>
2017-10-31 11:30:33 +0300

    add input with button

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -8,15 +8,15 @@ const App = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-sm-2 col-lg-4"></div>
-        <div className="col-sm-8 col-lg-4">
+        <div className="col-sm-2"></div>
+        <div className="col-sm-8">
           <div className="form-group">
           <h1>To Do App | React</h1>
           </div>
           <AddTaskBar />
           <TaskList />
         </div>
-        <div className="col-sm-2 col-lg-4"></div>
+        <div className="col-sm-2"></div>
       </div>
     </div>
   );
